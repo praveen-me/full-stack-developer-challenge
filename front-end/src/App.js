7309a3e3d9037765fd51d6a0a7cf069b29fd4d5a
praveen-me <praveenkrsaini0@gmail.com>
2019-02-13 11:40:53 +0530

    Add skeleton for styling and code splitting

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import sass
+import './scss/app.scss';
+
 import Header from './components/Header';
-import Stories from './components/Stories';
-import Login from './components/auth/Login';
-import SignUp from './components/auth/SignUp';
-import CreateStory from './components/story/CreateStory';
-import Profile from './components/Profile';
+
+const Stories = lazy(() => import('./components/Stories'));
+const Login = lazy(() => import('./components/auth/Login'));
+const SignUp = lazy(() => import('./components/auth/SignUp'));
+const CreateStory = lazy(() => import('./components/story/CreateStory'));
+const Profile = lazy(() => import('./components/Profile'));
+
 
 
 class App extends Component {
@@ -15,12 +20,14 @@ class App extends Component {
         <>
           <Header />
           <Switch>
-            <Route path='/' exact component={Stories} />
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={SignUp} />
-            <Route path='/profile/drafts/create' component={CreateStory}/>
-            <Route path='/profile' component={Profile}/>
-            <Route path='/edit/:id' component={CreateStory}/>
+            <Suspense fallback={'Loading...'}>
+              <Route path='/' exact component={Stories} />
+              <Route path='/login' component={Login} />
+              <Route path='/signup' component={SignUp} />
+              <Route path='/profile/drafts/create' component={CreateStory}/>
+              <Route path='/profile' component={Profile}/>
+              <Route path='/edit/:id' component={CreateStory}/>
+            </Suspense>
           </Switch>
         </>
       </BrowserRouter>
